d2b81e4f4d5c6cb81617f963d555e1de01fcf61b
TFyles <fylt1_13@uni.worc.ac.uk>
2014-10-21 20:05:54 +0100

    glkgkf
    fflf

--- www/js/index.js
+++ www/js/index.js
@@ -23,7 +23,7 @@ var launched_count = 0;
    
        var db = window.openDatabase("food",1.0,"food database", 1024 * 1024);
        db.transaction(createTable, error, success);
-
+ /*
        function createTable(tx){
        	tx.executeSql("CREATE TABLE IF NOT EXISTS nutrition(id UNIQUE, name TEXT, fat FLOAT)");
        }
@@ -53,6 +53,43 @@ var launched_count = 0;
              document.getElementById("res").innerHTML ="Row = " + i + " ID = " + results.rows.item(i).id + " Name =  " + results.rows.item(i).name; + " Fat = " + results.rows.item(i).fat ;
        	}
        }
+*/
+
+     function populateDB(tx) {
+        tx.executeSql('DROP TABLE IF EXISTS DEMO');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+        tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+        tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+    }
+
+    // Query the database
+    //
+    function queryDB(tx) {
+        tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+    }
+
+    // Query the success callback
+    //
+    function querySuccess(tx, results) {
+        var len = results.rows.length;
+        console.log("DEMO table: " + len + " rows found.");
+        for (var i=0; i<len; i++){
+            console.log("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data);
+        }
+    }
+
+    // Transaction error callback
+    //
+    function errorCB(err) {
+        console.log("Error processing SQL: "+err.code);
+    }
+
+    // Transaction success callback
+    //
+    function successCB() {
+        var db = window.openDatabase("Database", "1.0", "PhoneGap Demo", 200000);
+        db.transaction(queryDB, errorCB);
+    }
      
    }
 	
