1d1e05ef72eadcaff9fe1d5e46ddbbd1039d4f9d
TFyles <fylt1_13@uni.worc.ac.uk>
2014-10-21 19:42:21 +0100

    gggg
    ggg

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,37 @@ var launched_count = 0;
        };
 
        document.getElementById("ja").innerHTML = me.firstName+" "+ me.lastName+" "+me.course;
+   
+       var db = window.openDatabase("food",1.0,"food database", 1024 * 1024);
+       db.transaction(createTable, error, success);
+
+       function createTable(tx){
+       	tx.executeSql("CREATE TABLE IF NOT EXIST nutrition(id UNIQUE, name TEXT, fat FLOAT)");
+       }
+
+       function success(){
+       	db.transaction(addFood, error);
+       }
+
+       function addFood(tx){
+       	tx.executeSql("INSERT INTO nutrition(id, name, fat) VALUES (1, 'freddo', '28.5') ");
+       	tx.executeSql("INSERT INTO nutrition(id, name, fat) VALUES (2, 'snowbites', '20') ");
+       	tx.executeSql("INSERT INTO nutrition(id, name, fat) VALUES (3, 'snowman', '30.5') ");
+       }
+
+       function error(err) {
+       	alert("oops something went wrong " + err.message);
+       }
+
+       function queryDB(tx) {
+        tx.executeSql('SELECT * FROM nutrition', [], Success, error);
+       }
+       function querySuccess(tx, results) {
+        var len = results.rows.length;
+         document.getElementById("rows").innerHTML ="nutrition table: " + len + " rows found.";
+        for (var i=0; i<len; i++){
+             document.getElementById("results").innerHTML ="Row = " + i + " ID = " + results.rows.item(i).id + " Name =  " + results.rows.item(i).name; + " Fat = " + results.rows.item(i).fat ;
+       	}
    }
 	
 	function updateDisplay() {
